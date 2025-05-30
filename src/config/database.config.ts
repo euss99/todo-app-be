bef4c3b1d30ba09c73bea85363c886348d5619f5
Uriel Solis Salinas <euss99@hotmail.com>
2025-05-30 09:33:25 -0600

    feat: database configuration
    - Add TypeORM configuration
    - Setup PostgreSQL connection
    - Add database environment variables
    - Configure database entities structure

---
+++ src/config/database.config.ts
@@ -0,0 +1,15 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+import { TodoTypeOrmEntity } from "@/context/infrastructure/adapters/typeorm/todo.typeorm.entity";
+import { UserTypeOrmEntity } from "@/context/infrastructure/adapters/typeorm/user.typeorm.entity";
+
+export const databaseConfig: TypeOrmModuleOptions = {
+  type: "postgres",
+  host: process.env.DB_HOST || "localhost",
+  port: parseInt(process.env.DB_PORT!, 10) || 5432,
+  username: process.env.DB_USERNAME || "admin",
+  password: process.env.DB_PASSWORD || "123",
+  database: process.env.DB_DATABASE || "todo_app",
+  entities: [UserTypeOrmEntity, TodoTypeOrmEntity],
+  synchronize: process.env.NODE_ENV !== "production",
+};
